dda0bcffec7d68c59abd56ea84a3dbf6ed1ae069
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-12-16 18:33:16 +0100

    First commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import path from 'path'
+import express from 'express'
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static(publicPath))
+
+app.listen(port, () => console.log(`Server is up on port ${port}`))
